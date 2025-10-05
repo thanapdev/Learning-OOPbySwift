b1111233eade540939cf4549f9d25a78c9e66399
Thanapong Yamkamol <96330641+thanapdev@users.noreply.github.com>
2025-10-05 13:26:54 +0700

    ios_demo_1

---
+++ demo_ios_1/demo_ios_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demo_ios_1
+//
+//  Created by Thanapong Yamkamol on 1/9/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ demo_ios_1/demo_ios_1Tests/demo_ios_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  demo_ios_1Tests.swift
+//  demo_ios_1Tests
+//
+//  Created by Thanapong Yamkamol on 1/9/2568 BE.
+//
+
+import Testing
+@testable import demo_ios_1
+
+struct demo_ios_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
